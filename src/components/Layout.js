0f35710def9f13ae48cd234221083ee9a06b3ff0
Rickem <urielmric@gmail.com>
2021-12-20 11:48:07 +0100

    updates

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -43,6 +43,18 @@ function Layout() {
     fetchUsers();
   }
 
+  const getRoles = () =>{
+    const roles = ["Super admin", "Manager", "Agent", "Analiste"];
+    var random = Math.floor(Math.random()*roles.length);
+    return roles[random];
+  }
+
+  const getTwoStep = () =>{
+    const roles = ["Approved", "Declined", "In Progress", "In Transit"];
+    var random = Math.floor(Math.random()*roles.length);
+    return roles[random];
+  }
+
 
   
   useEffect(() => {
@@ -66,21 +78,29 @@ function Layout() {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-3">
+        <div className="col-3 vh-100">
           <Sidebar />
         </div>
-        <div className="col-9">
+        <div className="col-9 main-bg">
           <main className="p-8 mt-5">
-            <div className="page-title">
+            <div className="page-title mx-4">
               <h4>Users List</h4>
-              <p className="breadcumb d-flex align-items-center">
+              {/* <p className="breadcumb d-flex align-items-center">
                 <span>Home / </span>
                 <span className='ml-1'>User management / </span>
                 <span className='text-dark'> User List</span>
-              </p>
+              </p> */}
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="/" className="text-decoration-none">Home</a></li>
+                  <li class="breadcrumb-item"><a href="/" className="text-decoration-none">User management</a></li>
+                  <li class="breadcrumb-item active text-dark" aria-current="page">User List</li>
+                </ol>
+              </nav>
             </div>
+            
 
-            <section>
+            <section className="m-4">
               <div className="d-flex flex-row justify-content-between mb-4">
                 {/* Search box */}
                 <div className="search-box">
@@ -98,47 +118,58 @@ function Layout() {
               </div>
 
               {/* Display users in a table with 20 items per row */}
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th className='d-flex flex-row'>
-                      <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="custom-control-input" />
-                      </div>
-                      USER
-                    </th>
-                    <th>ROLE</th>
-                    <th>LAST LOGIN</th>
-                    <th>TWO STEP</th>
-                    <th>JOINED DATE</th>
-                  </tr>
-                </thead>
+              <div className="table-wrapper">
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th className='d-flex flex-row'>
+                        <div class="px-2">
+                          <input type="checkbox" class="custom-control-input" />
+                        </div>
+                        USER
+                      </th>
+                      <th>ROLE</th>
+                      <th>LAST LOGIN</th>
+                      <th>TWO STEP</th>
+                      <th>JOINED DATE</th>
+                    </tr>
+                  </thead>
 
-                <tbody>
-                  {users.map(user => (
-                    <tr key={user.id}>
-                      <td>
-                        <div className="d-flex flex-row align-items-center">
-                          <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" />
-                          </div>
+                  <tbody>
+                    {users.map(user => (
+                      <tr key={user.id}>
+                        <td>
                           <div className="d-flex flex-row align-items-center">
-                            <img src="https://cdn.pixabay.com/photo/2021/05/10/08/00/woman-6242836_960_720.png" alt="user" className="avatar-img rounded-circle" />
-                            <div className="user-info">
-                              <h6>{user.name}</h6>
-                              <p>{user.email}</p>
+                            <div class="mr-2 p-2">
+                              <input type="checkbox" class="custom-control-input" />
+                            </div>
+                            <div className="d-flex flex-row align-items-center">
+                              <div className='position-relative mr-3'>
+                                <img src="https://cdn.pixabay.com/photo/2021/05/10/08/00/woman-6242836_960_720.png" alt="user" className="avatar-img rounded-circle" />
+                                <span class="position-absolute bottom-0 start-100 translate-middle p-1 badge-green border border-light rounded-circle">
+                                  <span class="visually-hidden">Alerts</span>
+                                </span>
+                              </div>
+                              <div className="user-info px-4">
+                                <h6>{user.name}</h6>
+                                <p className='text-muted'>{user.email}</p>
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      </td>
-                      <td>Role</td>
-                      <td>Time ago</td>
-                      <td>{user.status === "active" ? "Yes" : "No"}</td>
-                      <td>05 May 2021</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+                        </td>
+                        <td className='custom-bold'>{ getRoles() }</td>
+                        <td>
+                          <span className='gray-pill'>Time ago</span>
+                        </td>
+                        <td>
+                          <TwoStepBox status={getTwoStep()} />
+                        </td>
+                        <td className='custom-bold'>05 May 2021</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
 
               {/* Pagination */}
 
@@ -170,3 +201,18 @@ function Layout() {
 }
 
 export default Layout
+
+function TwoStepBox({status}) {
+  switch (status) {
+    case "Approved":
+      return <span className='success-pill'>{ status }</span>
+    case "Declined":
+      return <span className='danger-pill'>{ status }</span>
+    case "In Progress":
+      return <span className='info-pill'>{ status }</span>
+    case "In Transit":
+      return <span className='warning-pill'>{ status }</span>
+    default:
+      break;
+  }
+}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,11 @@
 import { IconContext } from "react-icons"
-import { BsFillGridFill, BsShieldCheck } from "react-icons/bs"
+import { BsFillGridFill, BsShieldCheck, BsFillBellFill } from "react-icons/bs"
 import { HiUsers } from "react-icons/hi"
 import { FiBarChart2 } from "react-icons/fi"
 import { AiFillFileAdd  } from "react-icons/ai"
 import { ImBooks } from "react-icons/im"
 import { IoIosArrowForward } from "react-icons/io"
+import { Button } from "react-bootstrap"
 
 function Sidebar() {
   // const [active, setActive] = useState(false);
@@ -14,50 +15,72 @@ function Sidebar() {
       <div className="row">
         <div className="col-2">
           <div className="sidebar">
-            <div className="sidebar-icon icon-active">
-              <BsFillGridFill />
+            <div className="icons-start d-flex flex-column align-items-center">
+              <div className="sidebar-icon icon-active">
+                <BsFillGridFill />
+              </div>
+              
+              <div className="sidebar-icon">
+                <HiUsers />
+              </div>
+              <div className="sidebar-icon">
+                <FiBarChart2 />
+              </div>
+              <div className="sidebar-icon">
+                <BsShieldCheck />
+              </div>
+              <div className="sidebar-icon">
+                <ImBooks />
+              </div>
+              <div className="sidebar-icon">
+                <AiFillFileAdd />
+              </div>
             </div>
-            
-            <div className="sidebar-icon">
-              <HiUsers />
-            </div>
-            <div className="sidebar-icon">
-              <FiBarChart2 />
-            </div>
-            <div className="sidebar-icon">
-              <BsShieldCheck />
-            </div>
-            <div className="sidebar-icon">
-              <ImBooks />
-            </div>
-            <div className="sidebar-icon">
-              <AiFillFileAdd />
+
+            <div className="d-flex flex-column align-items-center">
+              <div className="sidebar-icon position-relative">
+                <BsFillBellFill />
+                <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+                  <span class="visually-hidden">6</span>
+                </span>
+              </div>
+              <div className="avatar-bg p-3 mt-2 rounded-circle d-flex align-items-center justify-content-center">
+                <img src="https://cdn.pixabay.com/photo/2021/05/10/08/00/woman-6242836_960_720.png" alt="user" className="avatar-img rounded-circle" />
+              </div>
             </div>
           </div>
         </div>
         <div className="col-10">
           <div className="p-4">
             <h4 className="text-dark mt-3">Comptes</h4>
-            <a className="sidemenu-item sidemenu-item-active" href="/">
-              Dashboard
-              <IoIosArrowForward />
-            </a>
-            <a className="sidemenu-item" href="/">
-              Sections
-              <IoIosArrowForward />
-            </a>
-            <a className="sidemenu-item" href="/">
-              Categories
-              <IoIosArrowForward />
-            </a>
-            <a className="sidemenu-item" href="/">
-              Labels
-              <IoIosArrowForward />
-            </a>
-            <a className="sidemenu-item" href="/">
-              Rôles
-              <IoIosArrowForward />
-            </a>
+            <div className="d-flex flex-column vh-100">
+              <div className="flex-grow-1">
+                <a className="sidemenu-item sidemenu-item-active" href="/">
+                  Dashboard
+                  <IoIosArrowForward />
+                </a>
+                <a className="sidemenu-item" href="/">
+                  Sections
+                  <IoIosArrowForward />
+                </a>
+                <a className="sidemenu-item" href="/">
+                  Categories
+                  <IoIosArrowForward />
+                </a>
+                <a className="sidemenu-item" href="/">
+                  Labels
+                  <IoIosArrowForward />
+                </a>
+                <a className="sidemenu-item" href="/">
+                  Rôles
+                  <IoIosArrowForward />
+                </a>
+              </div>
+              <div>
+
+              <Button className="w-100" variant="primary">Ajouter utilisateur</Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
